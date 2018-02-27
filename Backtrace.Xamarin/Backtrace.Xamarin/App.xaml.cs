25a1ed0f84a5f032b65d1215b1fbc78844daa7df
Konrad Dysput <konrad.dysput@hotmail.com>
2018-02-27 19:43:31 +0100

    Added Backtrace Client with sample credentials

--- Backtrace.Xamarin/Backtrace.Xamarin/App.xaml.cs
+++ Backtrace.Xamarin/Backtrace.Xamarin/App.xaml.cs
@@ -1,22 +1,66 @@
-﻿using System;
+﻿using Backtrace.Interfaces;
+using Backtrace.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 using Xamarin.Forms;
 
 namespace Backtrace.Xamarin
 {
 	public partial class App : Application
 	{
-		public App ()
+        private static BacktraceCredentials backtraceCredentials = new BacktraceCredentials(@"https://yolo.sp.backtrace.io:6098/", "328174ab5c377e2cdcb6c763ec2bbdf1f9aa5282c1f6bede693efe06a479db54");
+        private static BacktraceClient backtraceClient = new BacktraceClient(backtraceCredentials);
+
+        private static void DoSomething(int i = 0)
+        {
+            if (i == 2)
+            {
+                throw new ArgumentException("i");
+            }
+            Thread.Sleep(20);
+            try
+            {
+                DoSomething(++i);
+
+            }
+            catch (Exception e)
+            {
+                backtraceClient.Send(e);
+            }
+        }
+
+        public App ()
 		{
 			InitializeComponent();
 
 			MainPage = new Backtrace.Xamarin.MainPage();
-		}
 
-		protected override void OnStart ()
+
+            //Report a new exception from current application
+            try
+            {
+                Thread thread = new Thread(new ThreadStart(() => { DoSomething(0); }));
+                thread.Start();
+                thread.Join();
+                var i = 0;
+                var result = i / i;
+            }
+            catch (Exception exception)
+            {
+                var report = new BacktraceReport<object>(
+                    exception: exception,
+                    attributes: new Dictionary<string, object>() { { "AttributeString", "string" } },
+                    attachmentPaths: new List<string>() { @"path to file attachment", @"patch to another file attachment" }
+                );
+                backtraceClient.Send(report);
+            }
+
+        }
+
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
